6689dbf1783110537b8f424478172b764641ddd2
Asadbek Aytjanov <91543778+asadbekaytjanov@users.noreply.github.com>
2026-06-06 11:24:47 +0500

    unit tests are added

--- src/test/java/uz/aytjanov/googlephotosclone/GooglePhotosCloneApplicationTests.java
+++ src/test/java/uz/aytjanov/googlephotosclone/GooglePhotosCloneApplicationTests.java
@@ -3,11 +3,27 @@ package uz.aytjanov.googlephotosclone;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class GooglePhotosCloneApplicationTests {
 
     @Test
-    void contextLoads() {
+    void testPasswordValidation() {
+      String validPassword = "String12";
+      boolean isValid = validPassword.length() >= 8;
+      assertTrue(isValid, "Password should be at least 8 characters");
+    }
+    @Test
+    void testEmailValidation() {
+        String validEmail = "user@example.com";
+        boolean hasAtSymbol = validEmail.contains("@");
+        assertTrue(hasAtSymbol, "Email must contain @ symbol");
+    }
+    @Test
+    void testUploadValidation() {
+        String fileName = "file.jpg";
+        boolean isUploadValid = fileName.endsWith(".jpg") || fileName.endsWith(".png") || fileName.endsWith(".mp4") || fileName.endsWith(".mov");
+        assertTrue(isUploadValid, "You can upload only photos and videos");
     }
 
 }
